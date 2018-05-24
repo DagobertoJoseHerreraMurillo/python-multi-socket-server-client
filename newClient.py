ed38758471de5fe74e68f3832170b56d9c5af594
Sergio <36742793+sergiers3@users.noreply.github.com>
2018-05-24 14:48:55 +0200

    Add files via upload

---
+++ newClient.py
@@ -0,0 +1,39 @@
+import socket
+import sys
+import threading
+
+
+def code():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the port where the server is listening
+    server_address = ('localhost', 9999)
+    print('connecting to {} port {}'.format(*server_address))
+    sock.connect(server_address)
+
+    try:
+
+        # Send data
+        message = b'sergiers'
+        print('sending {!r}'.format(message))
+        sock.sendall(message)
+
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
+
+        while amount_received < amount_expected:
+            data = sock.recv(100)
+            amount_received += len(data)
+            msg = '{!r}'.format(data)
+            print("receive: "+msg[1:])
+            break
+    finally:
+        print('closing socket')
+        sock.close()
+
+
+for i in range(1):
+    hilo1 = threading.Thread(target=code)
+    hilo1.start()
